4f212cd9fe370bbc80a8612d9c669cb83b6b1f80
ayansaha1991 <ayansaha1991@gmail.com>
2016-09-05 20:19:37 +0530

    try2

--- Java8/src/com/ayan/Lamda/Lamdas.java
+++ Java8/src/com/ayan/Lamda/Lamdas.java
@@ -3,6 +3,7 @@ package com.ayan.Lamda;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Predicate;
 
 public class Lamdas {
 	
@@ -10,15 +11,19 @@ public class Lamdas {
 		return number > 3;
 	}
 	
+	
+	
 	public static void main(String[] args) {
 		
 		List<Integer> numbers = Arrays.asList(1,2,3,4,5);
-		
+		Predicate<Integer> isEven = e -> e%2 == 0;
 		Optional<Integer> result = numbers.stream()
 			.filter(Lamdas::isGT3)
-			.filter(e -> e % 2 == 0)
+			.filter(isEven)
 			.map(e -> e*2)
 			.findFirst();
+		
+		
 		
 		System.out.println(result);
 		
